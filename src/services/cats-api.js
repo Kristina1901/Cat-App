8b386bfeb33f484108c24deab596f0a33d863115
Kristina Tuzova <ktuzova1901@gmail.com>
2022-07-16 17:42:06 +0300

    feat: adding new function for query

--- src/services/cats-api.js
+++ src/services/cats-api.js
@@ -22,9 +22,9 @@ export async function getCatsBreeds() {
 
   return data;
 }
-export async function getCatsBreedsImg(limit, page) {
+export async function getCatsBreedsImg(limit, page, id) {
   const { data } = await axios.get(
-    `https://api.thecatapi.com/v1/breeds?limit=${limit}&page=${page}`,
+    `https://api.thecatapi.com/v1/breeds?limit=${limit}&page=${page}&breed_id=${id}`,
     {
       headers: {
         'Content-Type': 'application/json',
@@ -51,6 +51,19 @@ export async function getCatsBreedsbyName(query) {
 export async function getCatsBreedsImage(id, page) {
   const { data } = await axios.get(
     `https://api.thecatapi.com/v1/images/search?breed_id=${id}&page=${page}`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': '17d94b92-754f-46eb-99a0-65be65b5d18f',
+      },
+    }
+  );
+
+  return data;
+}
+export async function getCatsGallery(limit, img, order, page, breed) {
+  const { data } = await axios.get(
+    `https://api.thecatapi.com/v1/images/search?limit=${limit}&mime_types=${img}&order=${order}&size=small&page=${page}&breed_id=${breed}`,
     {
       headers: {
         'Content-Type': 'application/json',
