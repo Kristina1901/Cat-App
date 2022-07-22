ce442b796a626653b198fef8aeaa2d3fb5ced88b
Kristina Tuzova <ktuzova1901@gmail.com>
2022-07-22 12:56:41 +0300

    fix: class name changed

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,7 @@
 import s from 'components/Loader/Loader.module.css';
 const Loader = () => (
   <div className={s.loader}>
-    <div class={s.ldsRipple}>
+    <div className={s.ldsRipple}>
       <div></div>
       <div></div>
     </div>
